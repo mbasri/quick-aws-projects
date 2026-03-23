379dbc267aee96928bea8992226825fce0c8a9d5
Mohamed BASRI <mbasri.dev@outlook.com>
2026-03-23 19:30:39 +0100

    ci: restrict deployment environments and add second IAM profile
    Removes the pre-production (ppd) and production (prd) options from the deployment workflow to focus on the development environment. Additionally, introduces a second IAM instance profile resource to the infrastructure configuration.

--- quick-aws-ec2/07-iam-instance-profile.tf
+++ quick-aws-ec2/07-iam-instance-profile.tf
@@ -9,3 +9,11 @@ module "iam-instance-profile" {
 
   tags = local.tags
 }
+
+module "iam-instance-profile2" {
+  source = "git::https://github.com/mbasri-terraform-aws-modules/terraform-aws-iam-instance-profile?ref=v1"
+
+  iam_instance_profile_name = "${local.iam_instance_profile_name}-2"
+
+  tags = local.tags
+}
